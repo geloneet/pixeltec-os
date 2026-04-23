7349268c8b074bc868bfb6a5d9238da28653761d
Miguel Robles Sánchez <geloneet@gmail.com>
2026-04-23 18:52:08 +0000

    feat: add storage.googleapis.com remotePattern and getAdminStorage export

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -9,6 +9,7 @@
 
 import { initializeApp, getApps, cert, type App } from 'firebase-admin/app';
 import { getAuth, type Auth } from 'firebase-admin/auth';
+import { getStorage, type Storage } from 'firebase-admin/storage';
 
 let adminApp: App | undefined;
 
@@ -43,4 +44,8 @@ export { getAdminApp };
 
 export function getAdminAuth(): Auth {
   return getAuth(getAdminApp());
+}
+
+export function getAdminStorage(): Storage {
+  return getStorage(getAdminApp());
 }
